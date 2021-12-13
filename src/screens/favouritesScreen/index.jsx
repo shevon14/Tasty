28758a4bc13bf774ce7bddcc82815ac9bd9dee3b
Shevon Soyza <shevonrevinston@gmail.com>
2021-12-13 13:10:28 +0530

    favourites screen

--- src/screens/favouritesScreen/index.jsx
+++ src/screens/favouritesScreen/index.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  View, Text, FlatList, TouchableOpacity,
+} from 'react-native';
 import styles from './styles';
+import realm from '../../services/realm';
+import ResturentBox from '../../components/resturentBox';
+import searchResults from '../../api/searchResults';
+import { COLORS } from '../../constants/theme';
+import favouritesAction from '../../redux/action';
 
-const Favourites = () => (
-  <View style={styles.container}>
-    <Text>Favourites Screen</Text>
-  </View>
-);
+const Favourites = ({ navigation }) => {
+  const [searchApi, results, errorMsg] = searchResults();
+
+  const [data, setData] = useState([]);
+  const favourites = realm.objects('FavouritesSchema');
+
+  navigation.addListener('didFocus', () => {
+    data.splice(0, data.length);
+    const filterResults = (id) => results.filter((result) => result.id === id);
+    if (favourites) {
+      favourites.forEach((element) => {
+        const newFav = filterResults(element.restaurentId);
+        const alreadyExits = data.some((val) => val.id === element.restaurentId);
+        if (newFav[0] !== undefined && !alreadyExits) data.push(newFav[0]);
+      });
+      setData(data);
+    }
+  });
+
+  return (
+    <View
+      style={styles.container}
+    >
+      <View style={styles.topContainer}>
+        <Text style={styles.title}>
+          My Favourites
+        </Text>
+      </View>
+      <View>
+        <FlatList
+          data={data}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id}
+          numColumns={2}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('Details', { id: item.id });
+              }}
+            >
+              <ResturentBox data={item} />
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    </View>
+  );
+};
 
 export default Favourites;
--- src/screens/favouritesScreen/styles.jsx
+++ src/screens/favouritesScreen/styles.jsx
@@ -1,10 +1,22 @@
 import { StyleSheet } from 'react-native';
+import { COLORS, SIZES } from '../../constants/theme';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  topContainer: {
+    backgroundColor: COLORS.header,
+    height: 120,
+    paddingTop: 50,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: SIZES.h1 * 1.7,
+    fontWeight: 'bold',
+    color: COLORS.title,
+    paddingTop: 15,
+    paddingLeft: 10,
   },
 });
 
