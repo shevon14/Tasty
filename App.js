97c8d458235e950af3355a99e2904d0a87fdf4b0
Shevon Soyza <shevonrevinston@gmail.com>
2021-12-14 12:53:54 +0530

    App icon | Splash Screen

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import Router from './src/navigation/router';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Router />
   );
